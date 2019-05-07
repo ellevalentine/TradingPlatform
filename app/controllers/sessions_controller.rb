1f4007d733c0086ce7d472827316a3c74faf494c
Nahit-1 <nahit86@outlook.com>
2019-05-07 17:40:20 +0100

    working login page - currently redirects to /users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+    def new 
+    end 
+
+    def create 
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            flash[:success] = 'Succesfully Logged In!'
+            redirect_to '/users'
+        else  
+            flash[:warning] = "Invalid Username or Password"
+            redirect_to '/login'
+        end 
+    end
+    
+    def destroy 
+        session[:user_id] = nil 
+        flash[:success] = "Successfully Logged Out!"
+        redirect_to '/login'
+    end 
+end
