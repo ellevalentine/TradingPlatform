b2bc31fe292bcd3452285683d2dfd46efc86bae9
ellevalentine <elle.valentine@hotmail.com>
2019-05-09 22:07:36 +0100

    stocks updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             flash[:success] = 'Succesfully Logged In!'
-            redirect_to '/users'
+             redirect_to users_path
         else
             flash[:warning] = "Invalid Username or Password"
             redirect_to '/login'
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -26,8 +26,11 @@ class StocksController < ApplicationController
     @stock.update(quantity: params[:stock][:quantity])
 calc_quantity
     @user = User.find_by(id: session[:user_id]) || User.first
-    @tradingbook = Tradingbook.create(user: @user,  stock_id: @stock.id, company_name: @stock.company_name, quantity: @quantity, price: @stock.price , cost: ((@quantity*@stock.price)/100).to_f)
-    # @User.bank_account = @User.bank_account -
+    @tradingbook = Tradingbook.create(user: @user,  stock_id: @stock.id, company_name: @stock.company_name, quantity: @quantity, price: @stock.price , cost: ((@quantity*@stock.price)/100))
+
+    # @user.update(bank_account: params[:user][:bank_account])
+
+    # @stock.user.bank_account = @stock.user.bank_account - ((@quantity*@stock.price)/100)
     render :show
     # redirect_to tradingbook_path
   end
